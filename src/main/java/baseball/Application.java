5db23bdda921591911f14e3be6922a3a332816e4
Gun-Kenny <qkr0677@gmail.com>
2022-10-16 21:43:06 +0900

    feat: 게임 초기화 및 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        AppConfig appConfig = new AppConfig();
+        BaseBallGame baseBallGame = appConfig.of();
+        baseBallGame.play();
     }
 }
