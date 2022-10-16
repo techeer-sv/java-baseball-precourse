5c8e31f153b0b834b4ae937365f304afeaf70cc2
Gun-Kenny <qkr0677@gmail.com>
2022-10-16 21:42:49 +0900

    feat: 상대편 투수(컴퓨터) 클래스 생성
    기능
    - 숫자 랜덤 생성

---
+++ src/main/java/baseball/Opponent.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
+public class Opponent {
+    private final List<Integer> balls;
+
+    public Opponent() {
+        this.balls = new ArrayList<>();
+
+    }
+
+    public List<Integer> getBalls() {
+        return balls;
+    }
+
+    public void throwBall() {
+        while (balls.size() < 3) {
+            int ball = Randoms.pickNumberInRange(1,9);
+            if (!balls.contains(ball)) {
+                balls.add(ball);
+            }
+        }
+    }
+}
