fe5e8b96391a68c1a544e95e7f4ed695f1979cfb
Gun-Kenny <qkr0677@gmail.com>
2022-10-24 21:11:11 +0900

    feat: 예외 처리 로직 추가
    입력값의 유효성 검사
    1. 자리수 검증
    - 입력값은 세자리여야 함.
    2. 자연수 검증
    - 입력값은 자연수여야 함.
    3. 중복 검증
    - 입력값의 각 자리의 수는 서로 다른 수여야 함.
    입력값이 유효하지 않을 시 IllegalArgumentException 발생
    IllegalArgumentException은 play 메서드에서 catch 하여 에러 메세지를 출력한 후 반복문을 탈출한다.

--- src/main/java/baseball/Hitter.java
+++ src/main/java/baseball/Hitter.java
@@ -18,14 +18,55 @@ public class Hitter {
         if (this.balls.size() != 0) reset();
 
         StringTokenizer st = new StringTokenizer(Console.readLine());
+        String input = st.nextToken();
 
-        String ball = st.nextToken();
-        for (Character c : ball.toCharArray()) {
+        validate(input);
+
+        for (Character c : input.toCharArray()) {
             balls.add(Integer.parseInt(c.toString()));
         }
     }
 
     private void reset() {
         this.balls = new ArrayList<>();
+    }
+
+    private void validate(String input) {
+        if (!isValidSize(input)) throw new IllegalArgumentException("입력 값은 세자리 수여야 합니다.");
+        if (!isNumeric(input)) throw new IllegalArgumentException("입력 값은 자연수여야 합니다.");
+        if(isDuplicated(input)) throw new IllegalArgumentException("각 자리 수는 서로 다른 수여야 합니다.");
+    }
+
+    /**
+     * 입력 값의 자리수 검증
+     * @param input
+     * @return true if input size is valid.
+     */
+    private boolean isValidSize(String input) {
+        return input.length() == 3;
+    }
+
+    /**
+     * 입력값이 자연수임을 검증
+     * @param input
+     * @return true if input is digit.
+     */
+    private boolean isNumeric(String input) {
+        for (Character c : input.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * 각 자리 수는 서로 다른 수임을 검증
+     * @param input
+     * @return true if input's elements are duplicated each other.
+     */
+    private boolean isDuplicated(String input) {
+        for(int i=0; i<input.length(); i++) {
+            if (input.indexOf(input.charAt(i)) == i) return true;
+        }
+        return false;
     }
 }
